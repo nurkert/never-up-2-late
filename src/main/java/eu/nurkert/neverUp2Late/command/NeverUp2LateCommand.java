5445cb8098cfb8881b392fa85aed1902127e6b44
Niko <51508087+nurkert@users.noreply.github.com>
2025-10-02 14:45:20 +0200

    Add plugin overview chest GUI

--- src/main/java/eu/nurkert/neverUp2Late/command/NeverUp2LateCommand.java
+++ src/main/java/eu/nurkert/neverUp2Late/command/NeverUp2LateCommand.java
@@ -1,10 +1,12 @@
 package eu.nurkert.neverUp2Late.command;
 
+import eu.nurkert.neverUp2Late.gui.PluginOverviewGui;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
 
 import java.util.Collections;
 import java.util.List;
@@ -13,9 +15,11 @@ import java.util.Objects;
 public class NeverUp2LateCommand implements CommandExecutor, TabCompleter {
 
     private final QuickInstallCoordinator coordinator;
+    private final PluginOverviewGui overviewGui;
 
-    public NeverUp2LateCommand(QuickInstallCoordinator coordinator) {
+    public NeverUp2LateCommand(QuickInstallCoordinator coordinator, PluginOverviewGui overviewGui) {
         this.coordinator = Objects.requireNonNull(coordinator, "coordinator");
+        this.overviewGui = Objects.requireNonNull(overviewGui, "overviewGui");
     }
 
     @Override
@@ -25,17 +29,21 @@ public class NeverUp2LateCommand implements CommandExecutor, TabCompleter {
             return true;
         }
 
+        if (args.length == 0 || "gui".equalsIgnoreCase(args[0])) {
+            if (sender instanceof Player player) {
+                overviewGui.open(player);
+            } else {
+                sender.sendMessage(ChatColor.RED + "Die grafische Oberfläche kann nur von Spielern geöffnet werden.");
+            }
+            return true;
+        }
+
         if (args.length > 0 && "select".equalsIgnoreCase(args[0])) {
             if (args.length < 2) {
                 sender.sendMessage(ChatColor.RED + "Bitte gib die Nummer der gewünschten Datei an.");
                 return true;
             }
             coordinator.handleAssetSelection(sender, args[1]);
-            return true;
-        }
-
-        if (args.length == 0) {
-            sender.sendMessage(ChatColor.RED + "Verwendung: /" + label + " <url> oder /" + label + " select <nummer>");
             return true;
         }
 
@@ -51,6 +59,12 @@ public class NeverUp2LateCommand implements CommandExecutor, TabCompleter {
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        if (args.length == 1) {
+            return List.of("gui", "select");
+        }
+        if (args.length == 2 && "select".equalsIgnoreCase(args[0])) {
+            return Collections.singletonList("<nummer>");
+        }
         return Collections.emptyList();
     }
 }
